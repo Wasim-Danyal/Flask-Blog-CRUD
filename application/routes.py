89521cbb74ec5764d80bc73e59c0305c57a8f979
Wasim-Danyal <wasim.danyal1@gmail.com>
2020-07-23 15:44:14 +0100

    fixed layout.html

--- application/routes.py
+++ application/routes.py
@@ -3,10 +3,25 @@ from flask import render_template
 # import the app object from the ./application/__init__.py
 from application import app
 # define routes for / & /home, this function will be called when these are accessed
+
+blogData = [
+    {  
+        "name": {"first":"Tobi", "last":"Jackson"},
+        "title":"First Post",
+        "content":"My first blog post about Flask"
+    },
+    {   
+        "name": {"first":"Tobias", "last":"Jackson"},
+        "title":"Second Post",
+        "content":"This is even more blog data for Flask lectures"
+    }
+]
+
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', posts=blogData)
 
 @app.route('/about')
 def about():
