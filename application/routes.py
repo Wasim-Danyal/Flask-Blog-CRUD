7464d09c0e6155388ee7bc1feb51e63a54af16d4
Wasim-Danyal <wasim.danyal1@gmail.com>
2020-07-24 15:01:52 +0000

    updated v3, fixed post issue

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,10 @@
 # import render_template function from the flask module
-from flask import render_template
+from flask import render_template, redirect, url_for
 # import the app object from the ./application/__init__.py
-from application import app
+from application import app, db
 # define routes for / & /home, this function will be called when these are accessed
 from application.models import Posts
+from application.forms import PostForm
 @app.route('/')
 @app.route('/home')
 def home():
@@ -21,3 +22,25 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html', title='register')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
+
+
