39b8ca0744635d54b5af21cd9d15bd6e5f8f37e8
Wasim-Danyal <wasim.danyal1@gmail.com>
2020-07-25 13:21:04 +0000

    Added post and fixed navbar

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,9 @@ import os
 
 app = Flask(__name__)
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
 db = SQLAlchemy(app)
 
 from application import routes
